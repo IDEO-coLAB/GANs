410e63f4072d219dc7d53a7979d2646423f0edbf
Adam Lukasik <alukasik@cca.edu>
2018-06-15 12:03:51 -0700

    GAN output rendering in DOM

--- python/example.py
+++ python/example.py
@@ -1,20 +1,35 @@
+import tensorflow as tf
 import numpy as np
+import math
 from PIL import Image
 import hashlib
 
-width = 10
-height = 10
+from utils import pixels01
+from model import Model as M
+
+# Setup
+checkpoints_path = '/Users/alukasik/Documents/IDEO/Developer/checkpoints/chairs-30-5-18/checkpoint.ckpt'
 output_path = './public/imgs/'
+batch_size = 64 # must match batch size of saved session
+img_shape = (256, 256)
+zsize = 16 # number of variables for "genome"
+
+model = M(None, batch_size=batch_size, img_shape=img_shape, checkpoints_path=checkpoints_path, zsize=zsize)
+model.build_model()
+model.setup_session()
 
 def generate_image():
-    img = np.random.random_integers(0, high=255, size=[10,10,3]).astype('uint8')
-    # fimg = list(img.flatten())
-    # fimg = [int(x) for x in fimg]
+    zdraw = np.random.normal(scale=1.0, size=(batch_size, zsize)).astype('float32')
+    imgs = pixels01(model.Gz.eval(
+        { model.Z: zdraw, model.is_training: False },
+        session=model.sess))
+    img = imgs[0,:,:,:]
     hash = hashlib.sha1(img).hexdigest()
     hash = hash[:10]
     file_name = f'{hash}.jpg'
-    Image.fromarray(img).save(f'{output_path}/{file_name}')
+    img_asints = (img * 255.0).astype('uint8')
+    Image.fromarray(img_asints).save(f'{output_path}/{file_name}')
     return(file_name)
 
 def get_size():
-    return({'width': width, 'height': height})
+    return({'width': img_shape, 'height': img_shape})
